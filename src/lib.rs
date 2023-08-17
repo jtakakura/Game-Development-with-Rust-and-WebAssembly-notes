25e5cc0b2ca7d644d4f42241883d1b08d309351a
Junji Takakura <j.takakura@gmail.com>
2023-08-17 14:40:49 +0900

    ch01: draw a triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
 use utils::set_panic_hook;
 
@@ -10,8 +9,29 @@ use utils::set_panic_hook;
 pub fn main() -> Result<(), JsValue> {
     set_panic_hook();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.move_to(300.0, 0.0);
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     Ok(())
 }
