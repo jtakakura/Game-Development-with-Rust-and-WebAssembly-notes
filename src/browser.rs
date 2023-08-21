d21d56f480fb572fdb40bc0fd6830dd5144d0e79
Junji Takakura <j.takakura@gmail.com>
2023-08-21 17:40:11 +0900

    ch03: RequestAnimationFrame

--- src/browser.rs
+++ src/browser.rs
@@ -1,6 +1,10 @@
 use anyhow::{anyhow, Result};
 use futures::Future;
-use wasm_bindgen::{closure::WasmClosureFnOnce, prelude::Closure, JsCast, JsValue};
+use wasm_bindgen::{
+    closure::{WasmClosure, WasmClosureFnOnce},
+    prelude::Closure,
+    JsCast, JsValue,
+};
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
     CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlImageElement, Response, Window,
@@ -87,4 +91,20 @@ where
     F: 'static + WasmClosureFnOnce<A, R>,
 {
     Closure::once(fn_once)
+}
+
+pub fn closure_wrap<T: WasmClosure + ?Sized>(data: Box<T>) -> Closure<T> {
+    Closure::wrap(data)
+}
+
+pub type LoopClosure = Closure<dyn FnMut(f64)>;
+
+pub fn request_animation_frame(callback: &LoopClosure) -> Result<i32> {
+    window()?
+        .request_animation_frame(callback.as_ref().unchecked_ref())
+        .map_err(|err| anyhow!("Cannot request animation frame: {:#?}", err))
+}
+
+pub fn create_raf_closure(f: impl FnMut(f64) + 'static) -> LoopClosure {
+    closure_wrap(Box::new(f))
 }
