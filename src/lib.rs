36d4e50696f9660771f5aa2f9ba83b1e0bcc122b
Junji Takakura <j.takakura@gmail.com>
2023-08-22 19:38:46 +0900

    ch03: integrating the game loop

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,25 @@
 #[macro_use]
 mod browser;
 mod engine;
+mod game;
 mod utils;
 
-use std::collections::HashMap;
-use std::{rc::Rc, sync::Mutex};
-
-use serde::Deserialize;
-use wasm_bindgen::prelude::*;
-
+use engine::GameLoop;
+use game::WalkTheDog;
 use utils::set_panic_hook;
-
-#[derive(Deserialize)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
-}
-
-#[derive(Deserialize)]
-struct Cell {
-    frame: Rect,
-}
-
-#[derive(Deserialize)]
-struct Sheet {
-    frames: HashMap<String, Cell>,
-}
+use wasm_bindgen::prelude::*;
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     set_panic_hook();
 
-    let context = browser::context().expect("Could not get browser context");
-
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
-            .await
-            .expect("Could not fetch rhb.json")
-            .expect("Could not convert rhb.json into a Sheet structure");
+        let game = WalkTheDog::new();
 
-        let image = engine::load_image("rhb.png")
+        GameLoop::start(game)
             .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        interval_callback.forget();
+            .expect("Could not start game llop");
     });
 
     Ok(())
