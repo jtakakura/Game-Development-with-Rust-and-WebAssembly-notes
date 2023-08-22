05ad067166581cb4ade78a382abb950103ff24ef
Junji Takakura <j.takakura@gmail.com>
2023-08-22 18:12:31 +0900

    ch03: loading assets

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,7 @@
 use std::{cell::RefCell, rc::Rc, sync::Mutex};
 
 use anyhow::{anyhow, Result};
+use async_trait::async_trait;
 use futures::channel::oneshot::channel;
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
@@ -35,7 +36,9 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
     Ok(image)
 }
 
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&mut self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
     fn draw(&self, context: &CanvasRenderingContext2d);
 }
@@ -49,6 +52,7 @@ type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
     pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
