d0830bb969df2d5c1e2cbefded5255a9eb973c0c
Junji Takakura <j.takakura@gmail.com>
2023-09-03 16:37:44 +0900

    ch05: collision from below

--- src/game.rs
+++ src/game.rs
@@ -62,6 +62,14 @@ impl RedHatBoy {
         }
     }
 
+    fn pos_y(&self) -> i16 {
+        self.state_machine.context().position.y
+    }
+
+    fn velocity_y(&self) -> i16 {
+        self.state_machine.context().velocity.y
+    }
+
     fn frame_name(&self) -> String {
         format!(
             "{} ({}).png",
@@ -641,7 +649,11 @@ impl Game for WalkTheDog {
                 .bounding_box()
                 .intersects(&walk.platform.bounding_box())
             {
-                walk.boy.land_on(walk.platform.bounding_box().y);
+                if walk.boy.velocity_y() > 0 && walk.boy.pos_y() < walk.platform.position.y {
+                    walk.boy.land_on(walk.platform.bounding_box().y);
+                } else {
+                    walk.boy.knock_out();
+                }
             }
 
             if walk
