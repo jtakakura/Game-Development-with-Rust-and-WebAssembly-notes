37e737fa36ea3069493744f20a01239fc5ebde22
Junji Takakura <j.takakura@gmail.com>
2023-09-21 18:09:22 +0900

    ch07: adding audio to the engine

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc, sync::Mutex};
+use std::{array, cell::RefCell, collections::HashMap, rc::Rc, sync::Mutex};
 
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
@@ -8,9 +8,12 @@ use futures::channel::{
 };
 use serde::Deserialize;
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
-use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
+use web_sys::{AudioBuffer, AudioContext, CanvasRenderingContext2d, HtmlImageElement};
 
-use crate::browser::{self, LoopClosure};
+use crate::{
+    browser::{self, LoopClosure},
+    sound,
+};
 
 #[derive(Deserialize, Clone)]
 pub struct SheetRect {
@@ -352,4 +355,36 @@ impl SpriteSheet {
     pub fn draw(&self, renderer: &Renderer, source: &Rect, destination: &Rect) {
         renderer.draw_image(&self.image, source, destination);
     }
+}
+
+#[derive(Clone)]
+pub struct Audio {
+    context: AudioContext,
+}
+
+impl Audio {
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            context: sound::create_audio_context()?,
+        })
+    }
+
+    pub async fn load_sound(&self, filename: &str) -> Result<Sound> {
+        let array_buffer = browser::fetch_array_buffer(filename).await?;
+
+        let audio_buffer = sound::decode_audio_data(&self.context, &array_buffer).await?;
+
+        Ok(Sound {
+            buffer: audio_buffer,
+        })
+    }
+
+    pub fn play_sound(&self, sound: &Sound) -> Result<()> {
+        sound::play_sound(&self.context, &sound.buffer)
+    }
+}
+
+#[derive(Clone)]
+pub struct Sound {
+    buffer: AudioBuffer,
 }
