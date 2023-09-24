0ba244eb9ff9cb891ead70ae014d75b4357f85dc
Junji Takakura <j.takakura@gmail.com>
2023-09-24 11:36:52 +0900

    ch07: playing long music

--- src/game.rs
+++ src/game.rs
@@ -763,6 +763,8 @@ impl Game for WalkTheDog {
                 ));
                 let audio = Audio::new()?;
                 let sound = audio.load_sound("SFX_Jump_23.mp3").await?;
+                let background_music = audio.load_sound("background_song.mp3").await?;
+                audio.play_looping_sound(&background_music)?;
 
                 let rhb = RedHatBoy::new(json, engine::load_image("rhb.png").await?, audio, sound);
                 let background_width = background.width() as i16;
