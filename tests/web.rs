e888a088c4a463f57380e9e5715781b368e7cf79
Junji Takakura <j.takakura@gmail.com>
2023-10-04 21:14:52 +0900

    ch09: getting started

--- tests/web.rs
+++ tests/web.rs
@@ -1,13 +1,14 @@
-//! Test suite for the Web and headless browsers.
-
-#![cfg(target_arch = "wasm32")]
-
-extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
-
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 wasm_bindgen_test_configure!(run_in_browser);
 
+// This runs a unit test in native Rust, so it can only use Rust APIs.
+#[test]
+fn rust_test() {
+    assert_eq!(1, 1);
+}
+
+// This runs a unit test in the browser, so it can use browser APIs.
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn web_test() {
+    assert_eq!(1, 1);
 }
